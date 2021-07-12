6b8d539fda985f720ade75efb208adcad728f5a9
codeswithroh <codeswithrohl@example.com>
2021-07-13 00:03:57 +0530

    never mind

--- backend/src/handlers/create-polls.js
+++ backend/src/handlers/create-polls.js
@@ -1,15 +1,15 @@
-const {v4} = require('uuid')
+const {v6} = require('uuid')
 const { response } = require("express");
 
 module.exports = (db) => {
   return async (request, response) => {
       const data= {
-          _id: v4(),
+          _id: v6(),
           title: request.body.title,
           choices: request.body.choices.map(choice => ({
               name: choice,
               count:0,
-              _id: v4()
+              _id: v6()
           }))
       }
       await db.collection('polls').insertOne(data)
